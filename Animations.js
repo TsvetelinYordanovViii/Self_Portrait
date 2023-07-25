d6fa3b818d2ae279d7d5c4c538994f9eab87832a
Tsvetelin Yordanov <tsvetelin_yordanov_viii@abv.bg>
2023-07-25 10:38:43 +0300

    Added more comments. Cleared a bit of non-working css while at it. Potentially final commit unless I can add more comments.

--- Animations.js
+++ Animations.js
@@ -1,16 +1,24 @@
 let noseClick = document.querySelector(".noseButton");
 let shoulderClick = document.querySelector(".rightArmButton");
 
+//Since I wanted to make more complicated animations that's why I made this JavaScript file.
+
+
+//The idea of how an animation plays is as follows:
+//Button is pressed and then some elements get a class to make those parts animate.
+//After some time the class is removed, resetting the portrait.
 let slap = () => {
     let upperArm = document.querySelector(".left.upperArm");
     let lowerArm = document.querySelector(".left.lowerArm");
     let mouth = document.querySelector(".mouth");
+    //There are two irises and two pupils.
     let iris = document.querySelectorAll(".iris");
     let pupil = document.querySelectorAll(".pupil");
 
     upperArm.classList.add("slap");
     lowerArm.classList.add("slap");
     mouth.classList.add("slap");
+    //Didn't want to make a for cycle.
     iris[0].classList.add("slap");
     iris[1].classList.add("slap");
     pupil[0].classList.add("slap");
@@ -50,5 +58,6 @@ let wave = () => {
     }, 4000)
 }
 
+//Adding event listeners to the buttons.
 shoulderClick.addEventListener("click", wave);
 noseClick.addEventListener("click", slap);
